80de08b69f07a2dabaf573fc5e5e9439877bb02c
brabenetz <brabenetz@gmx.net>
2016-10-29 21:40:18 +0200

    update symlinks

--- archiv/updateLatest-SymLink.bat
+++ archiv/updateLatest-SymLink.bat
@@ -1,8 +1,11 @@
 rmdir latest
+rm latest
 mklink /D latest 1.0-SNAPSHOT
 rmdir current
+rm current
 mklink /D current 1.0-SNAPSHOT
 cd ..
 rmdir currentrelease
+rm currentrelease
 mklink /D currentrelease archiv\1.0-SNAPSHOT
 cd archiv
