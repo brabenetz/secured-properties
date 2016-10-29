91ba7fe4595bc9ff8a7f7149740c6752ebaf1732
brabenetz <brabenetz@gmx.net>
2016-10-29 14:45:58 +0200

    deactivate wrong validation (travis-ci supports AES-256)

--- src/test/java/net/brabenetz/lib/security/properties/core/EncryptionTest.java
+++ src/test/java/net/brabenetz/lib/security/properties/core/EncryptionTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,8 +37,8 @@ public class EncryptionTest {
         Assert.assertTrue(Encryption.isAlgorithmSupported(SupportedAlgorithm.DESede_168));
         Assert.assertFalse(Encryption.isAlgorithmSupported(new MockAlgorithm("test", 1)));
         Assert.assertFalse(Encryption.isAlgorithmSupported(new MockAlgorithm("AES", 1000)));
-        // By default, Java only supports AES 128, but you can import it into your jvm
-        Assert.assertFalse(Encryption.isAlgorithmSupported(SupportedAlgorithm.AES_256));
+        // By default, Java only supports AES 128 (travis-ci supports AES_256), but you can import it into your jvm
+        // Assert.assertFalse(Encryption.isAlgorithmSupported(SupportedAlgorithm.AES_256));
     }
 
     @Test
